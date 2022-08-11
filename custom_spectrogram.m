ecab2db7e6b8c8e3f896d1a25008ed7ff5f28abf
Yan Li <yan@edgeimpulse.com>
2022-08-11 14:23:15 -0700

    clean up matlab code

--- custom_spectrogram.m
+++ custom_spectrogram.m
@@ -1,16 +1,14 @@
 function [outputArg1] = custom_spectrogram(signal, output_graph_file_name, window, noverlap, fft)
 x = signal;
 
-h = figure('Visible','off');
+h = figure('Visible', 'off');
 
-[y,f,t,p] = spectrogram(x, window, noverlap, fft, 'centered');
-% figure()
-% y = spectrogram(x,32,16,64,'centered','yaxis');
+[y,f,t,p] = spectrogram(x, window, noverlap, fft, 1, 'centered');
 
-surf(t,f,10*log10(abs(p)),'EdgeColor','none');
+surf(t, f, 10*log10(abs(p)), 'EdgeColor', 'none');
 axis xy; axis tight; colormap(jet); view(0,90);
-xlabel('Time');
-ylabel('Frequency (Hz)');
+xlabel('Samples');
+ylabel('Normalized Frequency');
 
 if (~isempty(output_graph_file_name))
     print(h, '-dpng', output_graph_file_name);
